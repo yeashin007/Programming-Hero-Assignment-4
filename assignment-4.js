d1a11fa9d23cc18c71c3985e1434aa5dd40b3a26
yeashin007 <yeashin007@gmail.com>
2024-02-08 00:01:00 +0600

    Assingnment complete

--- assignment-4.js
+++ assignment-4.js
@@ -1,44 +1,41 @@
-// function calculateMoney(ticketSale) {
-//   if (ticketSale < 0) {
-//     return "Invalid Number";
-//   }
+/* Problem: 01 */
+function calculateMoney(ticketSale) {
+  if (ticketSale < 0) {
+    return "Invalid Number";
+  }
 
-//   const ticketPrice = 120;
-//   const totalSale = ticketPrice * ticketSale;
-//   const costPerDay = 500 + 8 * 50;
-//   const remainOnHand = totalSale - costPerDay;
+  const ticketPrice = 120;
+  const totalSale = ticketPrice * ticketSale;
+  const costPerDay = 500 + 8 * 50;
+  const remainOnHand = totalSale - costPerDay;
 
-//   return remainOnHand;
-// }
+  return remainOnHand;
+}
 
-// console.log(calculateMoney(-130));
+/* Problem: 02 */
 
-/* 2nd */
+function checkName(name) {
+  if (typeof name !== "string") {
+    return "invalid";
+  }
 
-// function checkName(name) {
-//   if (typeof name !== "string") {
-//     return "invalid";
-//   }
+  const lowerName = name.toLowerCase();
+  const strArr = ["A", "y", "i", "e", "o", "u", "w"];
+  const lowerStrArr = [];
 
-//   const lowerName = name.toLowerCase();
-//   const strArr = ["A", "y", "i", "e", "o", "u", "w"];
-//   const lowerStrArr = [];
+  for (let el of strArr) {
+    el = el.toLowerCase();
+    lowerStrArr.push(el);
+  }
 
-//   for (let el of strArr) {
-//     el = el.toLowerCase();
-//     lowerStrArr.push(el);
-//   }
+  if (lowerStrArr.includes(lowerName[lowerName.length - 1])) {
+    return "Good Name";
+  } else {
+    return "Bad Name";
+  }
+}
 
-//   if (lowerStrArr.includes(lowerName[lowerName.length - 1])) {
-//     return "Good Name";
-//   } else {
-//     return "Bad Name";
-//   }
-// }
-
-// console.log(checkName(["Rashed"]));
-
-/** Problem: 03 */
+/* Problem: 03 */
 function deleteInvalids(array) {
   if (!Array.isArray(array)) {
     return "Invalid Array";
@@ -55,4 +52,40 @@ function deleteInvalids(array) {
   return numberArray;
 }
 
-console.log(deleteInvalids({ num: [1, 2, 3] }));
+/* Problem: 04 */
+
+function password(obj) {
+  const birthYear = obj.birthYear;
+  const lengthOfBirthYear = String(birthYear).length;
+
+  if (!obj.name || !obj.birthYear || !obj.siteName || lengthOfBirthYear != 4) {
+    return "invalid";
+  }
+
+  const siteName = obj.siteName;
+  const firstLetterCapital = siteName[0].toUpperCase() + siteName.substring(1);
+
+  return `${firstLetterCapital}#${obj.name}@${obj.birthYear}`;
+}
+
+/* Problem: 05 */
+
+function monthlySavings(arr, livingCost) {
+  if (!Array.isArray(arr) || typeof livingCost !== "number") {
+    return "invalid input";
+  }
+
+  let totalIncome = 0;
+  for (const el of arr) {
+    const netIncome = el >= 3000 ? el - el * 0.2 : el;
+    totalIncome += netIncome;
+  }
+
+  const savings = totalIncome - livingCost;
+
+  if (savings >= 0) {
+    return savings;
+  } else {
+    return "earn more";
+  }
+}
